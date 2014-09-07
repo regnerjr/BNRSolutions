01b3d26e4854a4358e9acba8ce027302045de3bb
John Regner <john@johnregner.com>
2014-09-07 09:34:32 -0500

    Initial commit for HeavyRotation

---
+++ HeavyRotation/HeavyRotation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeavyRotation
+//
+//  Created by John Regner on 9/7/14.
+//  Copyright (c) 2014 In Your Dreams Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
