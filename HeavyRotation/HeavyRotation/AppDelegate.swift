68acf7fdfb70a042d751fe74eb21c154f28dd459
John Regner <john@johnregner.com>
2014-09-07 09:36:41 -0500

    Set up the window

--- HeavyRotation/HeavyRotation/AppDelegate.swift
+++ HeavyRotation/HeavyRotation/AppDelegate.swift
@@ -11,11 +11,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
                             
-    var window: UIWindow?
+    var window: UIWindow!
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+
+        window.backgroundColor = UIColor.whiteColor()
+        window.makeKeyAndVisible()
         return true
     }
 
