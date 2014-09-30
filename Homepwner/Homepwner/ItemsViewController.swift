6cf92251218abc4911ec5c5a6718f1331011df35
John <johnruby@Shelbys-MacBook.local>
2014-09-30 13:08:00 -0500

    Done trying to get the table view background image working for the Gold challenge. Will have to try this a different time. Moving on to chapter 10.

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ItemsViewController: UITableViewController {
 
     let cellIdentifier = "CELL"
-
+    
     convenience override init() {
         self.init(style: .Grouped)
         self.title = "Grouped"
@@ -40,6 +40,10 @@ extension ItemsViewController : UITableViewDataSource {
         
         //dequeue a cell or create a new one
         var cell = tableView.dequeueReusableCellWithIdentifier("CELL") as UITableViewCell! ?? UITableViewCell(style:.Default, reuseIdentifier: "CELL")
+        //set text to 20 pt
+        cell.textLabel?.font = UIFont.systemFontOfSize(20)
+        cell.backgroundView = nil
+        cell.backgroundColor = UIColor.clearColor()
         let sectionMethod =  indexPath.section == 0 ? BNRItemStore.getUnder50DollarItems : BNRItemStore.getOver50DollarItems
         
         let p = sectionMethod(BNRItemStore.sharedStore)()[indexPath.row]
@@ -50,29 +54,22 @@ extension ItemsViewController : UITableViewDataSource {
 
 extension ItemsViewController : UITableViewDelegate {
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let alert = UIAlertController(title: "Item selected", message: "You selected item \(indexPath.row)", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "OK",
-            style: UIAlertActionStyle.Default,
-            handler: {
-                (alert: UIAlertAction!) in println("An alert of type \(alert.style.hashValue) was tapped!")
-        }))
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
-    
     override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         if section == 0 {
             return 0
         }
-        return 40
+        return 44
     }
     override func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         if section == 0 {
             return nil
         }
-        let label =  UILabel(frame: CGRect(x: 0, y: 10, width: 200, height: 40))
+        let label =  UILabel(frame: CGRect(x: 0, y: 10, width: 200, height: 44))
         label.text = "No more items"
         return label
     }
     
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60
+    }
 }
