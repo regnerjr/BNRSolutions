5f27f3544408eeb824fc1c45bb0b84022ef64fa4
John Regner <john@johnregner.com>
2014-11-18 19:53:55 -0600

    Added stubs for Custom table view cell.

---
+++ Homepwner/Homepwner/HomepwnerItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomepwnerItemCell.swift
+//  Homepwner
+//
+//  Created by John Regner on 11/18/14.
+//  Copyright (c) 2014 In Your Dreams Software. All rights reserved.
+//
+
+import UIKit
+
+class HomepwnerItemCell: UITableViewCell {
+  
+}
