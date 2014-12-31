853bad4b1c15b7e1fb4614c2503d21397b566827
John Regner <john@johnregner.com>
2014-12-31 08:08:08 -0600

    Add Bind for optionals.

--- Homepwner/Homepwner/generic_functions.swift
+++ Homepwner/Homepwner/generic_functions.swift
@@ -18,3 +18,12 @@ func <*><A,B>(f:(A -> B)?, a: A?) -> B? {
   case .None: return .None
   }
 }
+
+//Define Bind for Optionals
+infix operator >>=- {associativity left}
+func >>=-<A,B>(a: A?, f: A -> B?) -> B? {
+  switch a {
+    case .Some (let x) : return f(x)
+    case .None : return nil
+  }
+}
