6cfec9458d4aab2596ecb3b5f9c2e322db4bb7c5
John Regner <john@johnregner.com>
2015-01-28 12:45:15 -0600

    Removed and Re-Added ViewController file which is actually needed.  Cleaned up unused functions in ViewController. Removed UnUsed Functions in the AppDelegate.

--- TouchTracker/TouchTracker/ViewController.swift
+++ TouchTracker/TouchTracker/ViewController.swift
@@ -2,14 +2,4 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-
 }
-
