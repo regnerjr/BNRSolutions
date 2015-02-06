80cdfa3c6e735ded086379c2de2e21f8087d14f3
John Regner <john@johnregner.com>
2015-02-06 17:07:52 -0600

    Added tableview controller.
    Setup to load navcontroller and tableviewcontroller in app delegate.

---
+++ NerdFeed/NerdFeed/ListVIewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class ListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+
+
+        return cell
+    }
+}
